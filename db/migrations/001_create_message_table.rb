ae0f3296c4881eeed95010ec868baa718f42dbe5
Josh Cass <joshuacass@gmail.com>
2015-05-25 08:19:42 -0600

    updated version

--- db/migrations/001_create_message_table.rb
+++
@@ -1,9 +0,0 @@
-require 'sequel'
-require_relative '../../collector'
-
-Collector.new.database.create_table :messages do
-                            primary_key :id
-                            String      :message
-                            String      :value
-end
-
